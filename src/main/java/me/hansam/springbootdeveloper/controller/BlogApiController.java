998e13b0e95c46a8f0a77535d83ce821722c642c
hsbae <hellospring89@gmail.com>
2024-06-07 16:34:48 +0900

    컨트롤러 생성

---
+++ src/main/java/me/hansam/springbootdeveloper/controller/BlogApiController.java
@@ -0,0 +1,28 @@
+package me.hansam.springbootdeveloper.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import me.hansam.springbootdeveloper.domain.Article;
+import me.hansam.springbootdeveloper.dto.AddArticleRequest;
+import me.hansam.springbootdeveloper.service.BlogService;
+
+@RequiredArgsConstructor
+@RestController //HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
+public class BlogApiController {
+    private final BlogService blogService;
+
+    //HTTP 메서드가 POST일 때 전달받은 URL과 동일하면 메서드로 매핑
+    @PostMapping("/api/articles")
+    // @RequestBody로 요청 본문 값 매핑
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(savedArticle);
+    }
+}
--- src/main/java/me/hansam/springbootdeveloper/domain/Article.java
+++ src/main/java/me/hansam/springbootdeveloper/domain/Article.java
@@ -1,11 +1,10 @@
 package me.hansam.springbootdeveloper.domain;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
