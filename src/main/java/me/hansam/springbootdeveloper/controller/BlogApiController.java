3f78693b6fc171411f3110aad460c919ca6f01d7
hsbae <hellospring89@gmail.com>
2024-06-09 21:45:37 +0900

    게시글 수정 기능 구현

--- src/main/java/me/hansam/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/hansam/springbootdeveloper/controller/BlogApiController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import me.hansam.springbootdeveloper.domain.Article;
 import me.hansam.springbootdeveloper.dto.AddArticleRequest;
 import me.hansam.springbootdeveloper.dto.ArticleResponse;
+import me.hansam.springbootdeveloper.dto.UpdateArticleRequest;
 import me.hansam.springbootdeveloper.service.BlogService;
 
 @RequiredArgsConstructor
@@ -60,5 +62,12 @@ public class BlogApiController {
         blogService.delete(id);
 
         return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id, @RequestBody UpdateArticleRequest request){
+        Article updateArticle = blogService.update(id, request);
+
+        return ResponseEntity.ok().body(updateArticle);
     }
 }
--- src/main/java/me/hansam/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/hansam/springbootdeveloper/service/BlogService.java
@@ -3,10 +3,12 @@ package me.hansam.springbootdeveloper.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import me.hansam.springbootdeveloper.domain.Article;
 import me.hansam.springbootdeveloper.dto.AddArticleRequest;
+import me.hansam.springbootdeveloper.dto.UpdateArticleRequest;
 import me.hansam.springbootdeveloper.repository.BlogRepository;
 
 @RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
@@ -33,5 +35,15 @@ public class BlogService {
     //블로그 글 삭제
     public void delete(long id){
         blogRepository.deleteById(id);
+    }
+
+    //블로그 글 수정
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request){
+        Article article = blogRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
