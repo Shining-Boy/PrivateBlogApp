76fe09bb96b52a66c5a4bcb61d476b9c45cb94b5
hsbae <hellospring89@gmail.com>
2024-06-09 14:48:03 +0900

    게시글 조회 기능 구현

--- src/main/java/me/hansam/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/hansam/springbootdeveloper/service/BlogService.java
@@ -23,4 +23,10 @@ public class BlogService {
     public List<Article> findAll() {
         return blogRepository.findAll();
     }
+
+    //블로그 글 조회
+    public Article findById(long id){
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
 }
